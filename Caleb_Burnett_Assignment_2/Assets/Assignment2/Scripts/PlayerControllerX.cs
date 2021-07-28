ad80edf32e05e5b088cc7856d10789dc1839194f
Caleb Burnett <calebburnett3.7@outlook.com>
2021-07-28 17:28:50 -0600

    Finished

---
+++ Caleb_Burnett_Assignment_2/Assets/Assignment2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool spawned = false;
+    private float decay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Reset();
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
+        {
+            decay = 1.5f;
+            spawned = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    private void Reset()
+    {
+        if (spawned && decay > 0)
+        {
+            decay -= Time.deltaTime;
+        }
+        if (decay < 0)
+        {
+            decay = 0;
+            spawned = false;
+        }
+    }
+
+    //IEnumerator sendDog()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    //}
+
+}
